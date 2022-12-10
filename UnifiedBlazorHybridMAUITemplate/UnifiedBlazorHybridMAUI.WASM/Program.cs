a60ab2c3837e54b6f739aa239109a7eca7c7a562
Jeremiah Weaver <jbw71699@gmail.com>
2022-12-09 21:10:01 -0600

    Moved solution into dedicated folder and added template to repo
    Moved solution into dedicated folder and added template to repo.

---
+++ UnifiedBlazorHybridMAUITemplate/UnifiedBlazorHybridMAUI.WASM/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Root;
+using Root.Data;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<Main>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddSingleton<WeatherForecastService>();
+
+await builder.Build().RunAsync();
+
